3dd1353891c406e845248c8cceef10693e288812
Sanija <ipb23.s.mitniece@vtdt.edu.lv>
2024-11-25 18:13:35 +0200

    mazas izmaiņas

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ echo "Strādā";
 $connection = new mysqli("localhost", "root","", "sipols");
 //$sql = "SLECT * FROM users"
 $result = $connection->query("SELECT * FROM users");
-$data = $result->fetch_assoc(); //assoc - tikai pirmā rindiņa, vien array
+$data = $result->fetch_assoc(); //assoc - tikai pirmā rindiņa, viens array
 
 //var_dump ($data["id"]);
 //echo "$data->id" . "$data->username";
